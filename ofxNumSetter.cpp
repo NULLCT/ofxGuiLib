7effdaeff32640edf0b380f200d1f64b1fc48548
NULLCT <cockatiel.u10@gmail.com>
2020-08-25 23:15:00 +0900

    ofxNumSetter pretty work

--- ofxNumSetter.cpp
+++ ofxNumSetter.cpp
@@ -13,18 +13,54 @@ void ofxNumSetter::reset() {
   num = 0;
 }
 
-void ofxNumSetter::draw() {
+void ofxNumSetter::run() {
   //draw twice triangle
   ofSetColor(arrowcolor);
-  ofDrawTriangle(x, y + h / 2, x + w * 0.2, y, x + w * 0.2, y + h);//left
+  if (isFocusRightArrow()) {
+    ofSetColor(arrowcolor.r + 10, arrowcolor.g + 10, arrowcolor.b + 10);
+    if (ofGetMousePressed()) {
+      if (!clicktrigger) {
+        clicktrigger = true;
+        num++;
+      }
+    }
+    else {
+      clicktrigger = false;
+    }
+  }
   ofDrawTriangle((x + w) - w * 0.2, y, (x + w) - w * 0.2, y + h, x + w, y + h / 2);//right
+
+  if (isFocusLeftArrow()) {
+    ofSetColor(arrowcolor.r + 10, arrowcolor.g + 10, arrowcolor.b + 10);
+    if (ofGetMousePressed()) {
+      if (!clicktrigger) {
+        clicktrigger = true;
+        num--;
+      }
+    }
+    else {
+      clicktrigger = false;
+    }
+  }
+  ofDrawTriangle(x, y + h / 2, x + w * 0.2, y, x + w * 0.2, y + h);//left
 
   //draw center box
   ofSetColor(255, 255, 255);
   ofDrawRectangle(x + w * 0.25, y, w * 0.5, h);
 
   //draw num
-  ofSetColor(0, 0, 0);
-  font.drawString(to_string(num), (w * 0.5) - font.stringWidth(to_string(num)) / 2 + x + w * 0.5,
-    (h + font.stringHeight(to_string(num)) / 2 + y) );
+  ofSetColor(100, 100, 100);
+  font.drawString(to_string(num), ((w * 0.5) - font.stringWidth(to_string(num))) / 2 + x + w * 0.25, (h + font.stringHeight(to_string(num))) / 2 + y );
+}
+
+bool ofxNumSetter::isFocusLeftArrow() {
+  return (x <= ofGetMouseX() and ofGetMouseX() <= x + w * 0.2) and (y <= ofGetMouseY() and ofGetMouseY() <= y + h);
+}
+
+bool ofxNumSetter::isFocusRightArrow() {
+  return ((x + w * 0.8) <= ofGetMouseX() and ofGetMouseX() <= x + w) and (y <= ofGetMouseY() and ofGetMouseY() <= y + h);
+}
+
+int ofxNumSetter::getNum() {
+  return num;
 }
--- ofxNumSetter.h
+++ ofxNumSetter.h
@@ -5,11 +5,15 @@ class ofxNumSetter {
 public:
   void set(int _x, int _y, int _w, int _h, ofColor& _arrowcolor, ofTrueTypeFont& _font);
   void reset();
-  void draw();
+  void run();
+  bool isFocusRightArrow();
+  bool isFocusLeftArrow();
+  int getNum();
 
 private:
   int num = 0;
   int x, y, w, h;
+  bool clicktrigger = false;
   ofColor arrowcolor;
   ofTrueTypeFont font;
 };
