a750bab3fbe94122c087d1cea4d8fda856150899
NULLCT <cockatiel.u10@gmail.com>
2020-08-25 21:22:05 +0900

    move some ofxButtons var to public

--- ofxButton.h
+++ ofxButton.h
@@ -8,6 +8,11 @@
 
 class ofxButton {
  public:
+  std::string text;
+  int x, y, w, h;
+  ofColor backcolor, wordcolor;
+  ofTrueTypeFont font;
+
   ofxButton();
   void set(int _x, int _y, int _w, int _h, ofColor& _backcolor,
            ofColor& _wordcolor, ofTrueTypeFont& _font, std::string _text);
@@ -15,10 +20,6 @@ class ofxButton {
   bool run(void);
 
  private:
-  int x, y, w, h;
-  ofColor backcolor, wordcolor;
-  ofTrueTypeFont font;
-  std::string text;
 
   bool clicktriger;
 };
